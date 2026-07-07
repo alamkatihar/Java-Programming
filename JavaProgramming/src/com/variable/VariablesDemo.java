d921c305629eb210ea32b65c8480410e8fc8fab3
Shahbaj Alam <shahbajktr2001@gmail.com>
2026-07-08 02:48:00 +0530

    Added Variables Demo Progra.

---
+++ JavaProgramming/src/com/variable/VariablesDemo.java
@@ -0,0 +1,21 @@
+package com.variable;
+
+public class VariablesDemo {
+
+	public static void main(String[] args) {
+		String name = "Shahbaj Alam";
+		int age = 25;
+		String course = "Java Full Stack";
+		double percentage = 82.50;
+
+		System.out.println("===== Student Details =====");
+
+		System.out.println("Name       : " + name);
+		System.out.println("Age        : " + age);
+		System.out.println("Course     : " + course);
+		System.out.println("Percentage : " + percentage);
+
+		System.out.println("===========================");
+	}
+
+}
