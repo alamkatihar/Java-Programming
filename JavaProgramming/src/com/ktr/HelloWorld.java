4198144c6c4c658bec6f57c0ab5a0dba55348bb2
Shahbaj Alam <shahbajktr2001@gmail.com>
2026-07-08 02:41:57 +0530

    init commit.

---
+++ JavaProgramming/src/com/ktr/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.ktr;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("=================================");
+		System.out.println("      Welcome to Java");
+		System.out.println("=================================");
+
+		System.out.println("Name    : Shahbaj Alam");
+		System.out.println("Course  : Java Full Stack");
+		System.out.println("Day     : 01");
+
+		System.out.println("---------------------------------");
+
+		System.out.println("Hello, World!");
+		System.out.println("Java is Platform Independent.");
+		System.out.println("Learning Java Step by Step.");
+
+		System.out.println("---------------------------------");
+		System.out.println("Thank You!");
+	}
+
+}
